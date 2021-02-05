3afb803f9849bc9e3bbb0b523c483151c442482f
Nathan Martin <nathanmartin5937@gmail.com>
2021-02-04 19:04:17 -0600

    fixed errors created required if statement

--- main.js
+++ main.js
@@ -1,68 +1,41 @@
 const students = [
-    {
-        name: 'Ryan',
-        groupHouse: 'Hufflepuff',
-        Age: 18,
-        imgUrl: 'https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg'
-    },
-    {
-        name: 'Andrew',
-        groupHouse: 'Gryffindor',
-        Age: 20,
-        imgUrl: 'https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg'
-    },
-    {
-        name: 'Kaily',
-        groupHouse: 'Ravenclaw',
-        Age: 21,
-        imgUrl: 'https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg'
-    },
-    {
-        name: 'Nathan',
-        groupHouse: 'Slytherin',
-        Age: 20,
-        imgUrl: 'https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg'
-    } 
+
 ];
 
 const groupHouses = [
-    {
-        name: 'Hufflepuff',
-        imgUrl: 'https://wizardingwonders.com/wp-content/uploads/harry-potter-hogwarts-house-flag-banner.jpg'
-    },
-    {
-        name: 'Gryffindor',
-        imgUrl: 'https://wizardingwonders.com/wp-content/uploads/harry-potter-hogwarts-house-flag-banner.jpg'
-    },
-    {
-        name: 'Ravenclaw',
-        imgUrl: 'https://wizardingwonders.com/wp-content/uploads/harry-potter-hogwarts-house-flag-banner.jpg'
-    },
-    {
-        name: 'Slytherin',
-        imgUrl: 'https://wizardingwonders.com/wp-content/uploads/harry-potter-hogwarts-house-flag-banner.jpg'
-    } 
+    
+        'Hufflepuff',
+        // imgUrl: 'https://wizardingwonders.com/wp-content/uploads/harry-potter-hogwarts-house-flag-banner.jpg'
+    
+        'Gryffindor',
+        // imgUrl: 'https://wizardingwonders.com/wp-content/uploads/harry-potter-hogwarts-house-flag-banner.jpg'
+    
+        'Ravenclaw',
+        // imgUrl: 'https://wizardingwonders.com/wp-content/uploads/harry-potter-hogwarts-house-flag-banner.jpg'
+    
+        'Slytherin',
+    //     imgUrl: 'https://wizardingwonders.com/wp-content/uploads/harry-potter-hogwarts-house-flag-banner.jpg'
+
 ];
-// A function to select the inerr html (form, id)
+
 const printToDom = (divId, textToDom) => {
     const selectedForm = document.querySelector(divId);
     selectedForm.innerHTML = textToDom;
 };
-// A function to handle the "get started" btn click to target the form html
 
-const cardBuilder = (e) => {
+const hatBuilder = (taco) => {
     let studentDom = "";
-    e.forEach((item, i) => {
-     
+    taco.forEach((item, i) => {
         studentDom += `<div id=${i} class="card">
               <div class="card-body">
-                    <p class="cardText">${item.groupHouse}</p>
+                    <p class="cardText">${item.house}</p>
                     <p class="cardName">${item.name}</p>
-                    <button type="btn" id=${i} class="btn btn-primary" id="expelBtn">EXPELLED</button>
+                    <button type="button" id=${i} class="btn btn-primary" id="expelBtn">EXPELLED</button>
               </div>
             </div>`
     });
-printToDom("#cardContainer", studentDom);
+
+printToDom("#students", studentDom);
 };
 
 const handleBtnClick = (e) => {
@@ -72,37 +45,45 @@ const handleBtnClick = (e) => {
     }        
 };
 
-// selecting btn id adding click event listener 
 const pullForm = (e) => {
     e.preventDefault();
 
     const name = document.querySelector("#student-name").value;
     const randomHouseGroup = Math.floor(Math.random() * groupHouses.length);
-    const houseGroup = groupHouses[randomHouseGroup];
+    const house = groupHouses[randomHouseGroup];
 
     const studentObject = {
     name,
-    houseGroup,
+    house,
 };
 
+const helpfulForm = () => {
+    let reminder = `<p class="text-white">Name required.</p>`;
+    printToDom('#requiredMessage', reminder);
+  }
+  if (name.length === 0) {
+      helpfulForm();
+  } else {
     students.push(studentObject);
-    cardBuilder(students);
+  }
 
-    document.querySelector("infoForm").reset = "";
+    hatBuilder(students);
+
+    document.querySelector("form").reset();
 };
 
 const cardRemoval = (e) => {
     const targetType = e.target.type;
     const targetId = Number(e.target.id);
   
-            if (targetType === "btn") {
+            if (targetType === "button") {
             const studentIndex = students.findIndex(
                 (student) => student.id === targetId
             );
             students.splice(studentIndex, 1);
             }
   
-    cardBuilder(students);
+    hatBuilder(students);
   };
 
 const btnEvents = () => {
@@ -111,16 +92,9 @@ const btnEvents = () => {
     document.querySelector("#students").addEventListener("click", cardRemoval);
 };
 
-// created a function to handle all btn events 
-
 const init = () => {
     btnEvents();
-    cardBuilder(students);
+    hatBuilder(students);
   };
   
   init();
-
-
-// make objects
-// push obj pass array
-
